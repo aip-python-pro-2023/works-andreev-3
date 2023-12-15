245351f69a55714b803807300553fdb5dcd6b2ac
Nikolay Andreev <bakasaru@list.ru>
2023-12-15 18:14:01 +0300

    feat: add getters/setters and props

--- quest.py
+++ quest.py
@@ -1,5 +1,4 @@
 import json
-from random import randint
 
 
 class Quest:
@@ -7,19 +6,44 @@ class Quest:
 
     def __init__(self, quest_id, name, description, difficulty, steps):
         Quest.count += 1
-        self.id = quest_id
-        self.name = name
-        self.description = description
-        self.difficulty = difficulty
-        self.steps = steps
+        self.__id = quest_id
+        self.__name = name
+        self.__description = description
+        self.__difficulty = difficulty
+        self.__steps = steps
+
+    def get_name(self):
+        return self.__name
+
+    # Геттер
+    def get_description(self):
+        return self.__description  # .replace('<ToPlanet>', 'Марс').replace('<ToStar>', 'Солнце')
+
+    # Сеттер
+    def set_description(self, new_description):
+        if isinstance(new_description, str) and new_description != '':
+            self.__description = new_description
+        else:
+            print('Wrong description')
+
+    @property
+    def description(self):
+        return self.__description  # .replace('<ToPlanet>', 'Марс').replace('<ToStar>', 'Солнце')
+
+    @description.setter
+    def description(self, value):
+        if isinstance(value, str) and value != '':
+            self.__description = value
+        else:
+            print('Wrong description')
 
     # Преобразование в строчку
     def __str__(self):
-        return f'Quest(id={self.id}, name={self.name}, difficulty={self.difficulty})'
+        return f'Quest(id={self.__id}, name={self.__name}, difficulty={self.__difficulty})'
 
     # Проверка на равенство
     def __eq__(self, other):
-        return isinstance(other, Quest) and self.id == other.id
+        return isinstance(other, Quest) and self.__id == other.__id
 
     def next_step(self, current_step, state):
         if state == 0:
@@ -28,7 +52,7 @@ class Quest:
             return 'SUCCESS'
 
     def mark_as_passed(self, user):
-        user.passed_quests.append(self.id)
+        user.passed_quests.append(self.__id)
 
     @staticmethod
     def create_from_json(filename):
@@ -47,12 +71,16 @@ class Quest:
 class User:
     def __init__(self, user_id):
         self.id = user_id
+        self.__name = 'Test'
         self.passed_quests = []
 
-    def __contains__(self, item):
-        if isinstance(item, Quest):
-            return item.id in self.passed_quests
-        return False
+    def get_name(self):
+        return self.__name
+
+    # def __contains__(self, item):
+    #     if isinstance(item, Quest):
+    #         return item.__id in self.passed_quests
+    #     return False
 
 
 iikebana = Quest(
@@ -75,12 +103,12 @@ print(iikebana)
 print(diehard)
 print(iikebana == diehard)
 
-user = User('Moi')
+user = User('Nick')
 print(iikebana == user)
 
-user.passed_quests.append(iikebana.id)
-if iikebana in user:
-    print('Passed!')
+# user.passed_quests.append(iikebana.__id)
+# if iikebana in user:
+#     print('Passed!')
 
 print(Quest.count)
 # diehard.count = diehard.count + 1
@@ -90,4 +118,12 @@ print(Quest.count)
 
 gobsaur = Quest.create_from_json('quests/gobsaur.json')
 print(gobsaur)
+print(gobsaur.get_description())
+
+gobsaur.description += '\n\nДополнительное описание'
 print(gobsaur.description)
+# gobsaur.set_description(gobsaur.get_description() + '\n\nДополнительное описание')
+# print(gobsaur.get_description())
+
+for item in gobsaur, user:
+    print(item.get_name())
